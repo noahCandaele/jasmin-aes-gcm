ded99fb1a45d22b13fefaf8c4dea638f1e9d4dbd
Anthony IOZZIA <anthony.iozzia@etu.univ-cotedazur.fr>
2023-12-14 10:00:55 +0100

    add example c functions for aes_gcm

---
+++ aes_gcm_c/aes_gcm.c
@@ -0,0 +1,27 @@
+#include "aes_gcm.h"
+#include "aes.h"
+
+// Initialization logic for AES-GCM context, key expansion, IV setup, etc.
+void aes_gcm_init(AES_GCM_Context *ctx, const unsigned char *key, size_t key_len, const unsigned char *iv, size_t iv_len) {
+    
+}
+
+// AES-GCM encryption logic
+void aes_gcm_encrypt(AES_GCM_Context *ctx, const unsigned char *plaintext, size_t plaintext_len, unsigned char *ciphertext, unsigned char *tag) {
+    
+}
+
+// AES-GCM decryption logic
+void aes_gcm_decrypt(AES_GCM_Context *ctx, const unsigned char *ciphertext, size_t ciphertext_len, unsigned char *plaintext, unsigned char *tag) {
+    
+}
+
+// Authentication tag calculation logic
+void calculate_authentication_tag(AES_GCM_Context *ctx, unsigned char *tag) {
+    
+}
+
+    // Verify authentication tag logic
+int verify_authentication_tag(const unsigned char *expected_tag, const unsigned char *tag) {
+   
+}
---
+++ aes_gcm_c/aes_gcm.h
@@ -0,0 +1,14 @@
+#ifndef AES_GCM_H
+#define AES_GCM_H
+
+typedef struct {
+    // Add necessary variables to hold state, keys, IV, etc.
+} AES_GCM_Context;
+
+void aes_gcm_init(AES_GCM_Context *ctx, const unsigned char *key, size_t key_len, const unsigned char *iv, size_t iv_len);
+void aes_gcm_encrypt(AES_GCM_Context *ctx, const unsigned char *plaintext, size_t plaintext_len, unsigned char *ciphertext, unsigned char *tag);
+void aes_gcm_decrypt(AES_GCM_Context *ctx, const unsigned char *ciphertext, size_t ciphertext_len, unsigned char *plaintext, unsigned char *tag);
+void calculate_authentication_tag(AES_GCM_Context *ctx, unsigned char *tag);
+int verify_authentication_tag(const unsigned char *expected_tag, const unsigned char *tag);
+
+#endif
