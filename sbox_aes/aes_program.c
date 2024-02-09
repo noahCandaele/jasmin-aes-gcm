79ec0f50b791fdeb5cc430683baea11140a20973
Anthony IOZZIA <iozzia.anthony@gmail.com>
2024-02-09 09:41:27 +0100

    Fix bugs in sbox

--- sbox_aes/aes_program.c
+++ sbox_aes/aes_program.c
@@ -1,30 +1,39 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-extern void init_aes_sbox(uint64_t myList);
+#define SBOX_SIDE 16
+
+extern void init_aes_sbox(uint64_t* sbox);
 
 int main() {
-    const int side = 16;
-    uint64_t my_sbox[side*side];
+    size_t array_size = SBOX_SIDE * SBOX_SIDE * sizeof(uint64_t);
+    uint64_t *my_sbox = (uint64_t *)malloc(array_size);
+
+    if (my_sbox == NULL) {
+        // Allocation failed, handle the error
+        return 1;
+    }
 
     // Initialize the array to 0
-    for (int i = 0; i < side * side; ++i) {
+    for (int i = 0; i < SBOX_SIDE * SBOX_SIDE; ++i) {
         my_sbox[i] = 0;
     }
 
-    printf("Size: %ld\n", sizeof(my_sbox)/sizeof(uint64_t));
+    // printf("Size: %ld\n", sizeof(my_sbox)/sizeof(uint64_t));
     printf("Address of the array: %p\n", my_sbox);
     // Call the Jasmin function to change the element at index 3 to 42
-    init_aes_sbox(*my_sbox);
+    init_aes_sbox(my_sbox);
 
     // Print the modified array
-    for (int line = 0; line < side; line++) {
+    for (int line = 0; line < SBOX_SIDE; line++) {
         // printf("current line %d\n", line);
-        for (int col = 0; col<side; col++){
-            printf("%02lx ", my_sbox[col+side*line]);
+        for (int col = 0; col<SBOX_SIDE; col++){
+            printf("%02lx ", my_sbox[col+SBOX_SIDE*line]);
         }
         printf("\n");
     }
 
+    free(my_sbox);
     return 0;
 }
