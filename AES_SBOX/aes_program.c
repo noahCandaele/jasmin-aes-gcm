503b33177f8ee116ee4a35fc2d170a04605bd1aa
noahCandaele <noah.candaele@gmail.com>
2024-02-09 09:09:48 +0100

    add prototype sbox and array overflow

---
+++ AES_SBOX/aes_program.c
@@ -0,0 +1,30 @@
+#include <stdint.h>
+#include <stdio.h>
+
+extern void init_aes_sbox(uint64_t myList);
+
+int main() {
+    const int side = 16;
+    uint64_t my_sbox[side*side];
+
+    // Initialize the array to 0
+    for (int i = 0; i < side * side; ++i) {
+        my_sbox[i] = 0;
+    }
+
+    // print the address of the array
+    printf("Address of the array: %p\n", my_sbox);
+    // Call the Jasmin function to change the element at index 3 to 42
+    init_aes_sbox(*my_sbox);
+
+    // Print the modified array
+    for (int line = 0; line < side; line++) {
+        // printf("current line %d\n", line);
+        for (int col = 0; col<side; col++){
+            printf("%02lx ", my_sbox[col+side*line]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ array_overflow/test.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+// define de la size du tableau
+#define SIZE 16
+
+extern unsigned long long main_function();
+
+int main() {
+    // Initialize the list
+    unsigned char list[SIZE] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+    
+    // Call the assembly function
+    printf("Modified List:\n");
+    for (int i = 0; i < SIZE; i++) {
+        printf("%d ", list[i]);
+    }
+    main_function(list);
+
+    printf("\n");
+    // Print the modified list
+    printf("Modified List:\n");
+    for (int i = 0; i < SIZE; i++) {
+        printf("%d ", list[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
