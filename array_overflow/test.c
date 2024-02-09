16a387481cf9caa9eb6bc11cccefcc44f83d5080
Anthony IOZZIA <iozzia.anthony@gmail.com>
2024-02-09 12:48:54 +0100

    Fix sbox

--- array_overflow/test.c
+++ array_overflow/test.c
@@ -3,11 +3,22 @@
 // define de la size du tableau
 #define SIZE 16
 
-extern unsigned long long main_function();
+extern unsigned long main_function(char* list);
 
 int main() {
     // Initialize the list
     unsigned char list[SIZE] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+    size_t list_size = SIZE * SIZE * sizeof(char);
+    unsigned char *list = (char *)malloc(list_size);
+
+    if (my_sbox == NULL) {
+        // Allocation failed, handle the error
+        return 1;
+    }
+
+    for (int i = 0; i < SIZE; i++) {
+        printf("%d ", list[i]);
+    }
     
     printf("Initial list:\n");
     for (int i = 0; i < SIZE; i++) {
@@ -24,5 +35,6 @@ int main() {
     }
     printf("\n");
 
+    free(list);
     return 0;
 }
--- sbox_aes/aes_program.c
+++ sbox_aes/aes_program.c
@@ -1,14 +1,17 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <inttypes.h>
 
 #define SBOX_SIDE 16
 
-extern void init_aes_sbox(uint64_t* sbox);
+// extern void init_aes_sbox(uint64_t* sbox);
+extern void compute_array(uint8_t* sbox);
+extern uint8_t sbox_get(uint64_t i);
 
 int main() {
-    size_t array_size = SBOX_SIDE * SBOX_SIDE * sizeof(uint64_t);
-    uint64_t *my_sbox = (uint64_t *)malloc(array_size);
+    size_t array_size = SBOX_SIDE * SBOX_SIDE * sizeof(uint8_t);
+    uint8_t *my_sbox = (uint8_t *)malloc(array_size);
 
     if (my_sbox == NULL) {
         // Allocation failed, handle the error
@@ -23,13 +26,15 @@ int main() {
     // printf("Size: %ld\n", sizeof(my_sbox)/sizeof(uint64_t));
     printf("Address of the array: %p\n", my_sbox);
     // Call the Jasmin function to change the element at index 3 to 42
-    init_aes_sbox(my_sbox);
+    compute_array(my_sbox);
+    // uint8_t res = sbox_get(255);
+    // printf("res: %" PRIu8 "\n",res);
 
     // Print the modified array
     for (int line = 0; line < SBOX_SIDE; line++) {
         // printf("current line %d\n", line);
         for (int col = 0; col<SBOX_SIDE; col++){
-            printf("%02lx ", my_sbox[col+SBOX_SIDE*line]);
+            printf("%" PRIx8 " ", my_sbox[col+SBOX_SIDE*line]);
         }
         printf("\n");
     }
