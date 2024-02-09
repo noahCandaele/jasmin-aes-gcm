0c512b3e7baf8b9de363491f2483c7e35db711ab
Anthony IOZZIA <iozzia.anthony@gmail.com>
2024-02-09 09:16:30 +0100

    Clean example array overflow

--- array_overflow/test.c
+++ array_overflow/test.c
@@ -9,16 +9,16 @@ int main() {
     // Initialize the list
     unsigned char list[SIZE] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
     
-    // Call the assembly function
-    printf("Modified List:\n");
+    printf("Initial list:\n");
     for (int i = 0; i < SIZE; i++) {
         printf("%d ", list[i]);
     }
+    // Call the assembly function
     main_function(list);
 
     printf("\n");
     // Print the modified list
-    printf("Modified List:\n");
+    printf("Modified list:\n");
     for (int i = 0; i < SIZE; i++) {
         printf("%d ", list[i]);
     }
