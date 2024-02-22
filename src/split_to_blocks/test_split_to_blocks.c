29867700adb068daf650b8d3be3003a8797095b7
Anthony IOZZIA <iozzia.anthony@gmail.com>
2024-02-22 10:52:25 +0100

    More thorough tests for split blocks

--- src/split_to_blocks/test_split_to_blocks.c
+++ src/split_to_blocks/test_split_to_blocks.c
@@ -6,152 +6,135 @@
 
 #include "../utils/utils.h"
 
-extern void get_block_exp(uint8_t* strin, uint8_t* strout, int len, int blockID);
+extern void get_block_export(uint8_t* data, uint8_t* out_block, int data_length, int block_id);
 
-int test1() {
-	printf("######## Test 1 get block 0, size 1 block ########\n");
+int test_generic(char* data_str, int block_id, char* block_str_expected) {
+	// Data
+	size_t data_length = strlen(data_str);
+	// printf("data length %zu\n", data_length);
+	uint8_t data[data_length];
+	convert_ascii_string_to_uint8_array(data_str, data, data_length);
 
-	uint8_t arr_in[] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10};
-	uint8_t arr_out[NB_BYTES_128_BITS];
+	// Expected block
+	size_t block_length_expected = strlen(block_str_expected);
+	// printf("block length expected %zu\n", block_length_expected);
+	uint8_t block_expected[block_length_expected];
+	convert_ascii_string_to_uint8_array(block_str_expected, block_expected, block_length_expected);
 
-	get_block_exp(arr_in, arr_out, NB_BYTES_128_BITS, 0);
-
-	printf("Expected output: "); print_uint8_array_as_hex(arr_in, NB_BYTES_128_BITS, true);
-	printf("Actual output:   "); print_uint8_array_as_hex(arr_out, NB_BYTES_128_BITS, true);
-
-	if(!compare_uint8_arrays(arr_in, arr_out, NB_BYTES_128_BITS)) {
+	// Get block
+	uint8_t block[block_length_expected];
+	get_block_export(data, block, data_length, block_id);
+	
+	// Print output
+	printf("Expected output: %s\n", block_str_expected);
+	printf("Actual output:   "); print_uint8_array_as_ascii(block, block_length_expected, false);
+	
+	// Compare
+	if(!compare_uint8_arrays(block, block_expected, block_length_expected)) {
 		return CODE_FAIL;
 	}
 
 	return CODE_SUCCESS;
 }
 
-int test2() {
-	printf("######## Test 2 get block 0, size 1 block ########\n");
-
-	char str[] = "ABCDEFGHIJKLMNOP";
-	size_t size = strlen(str);
-	printf("size %zu\n", size);
-	uint8_t arr_in[size];
-	convert_ascii_string_to_uint8_array(str, arr_in, size);
-	uint8_t arr_out[NB_BYTES_128_BITS];
-
-	get_block_exp(arr_in, arr_out, size, 0);
-
-	printf("Expected output: ABCDEFGHIJKLMNOP\n");
-	printf("Actual output:   "); print_uint8_array_as_ascii(arr_out, NB_BYTES_128_BITS, false);
-	
-	char str_out[NB_BYTES_128_BITS];
-	convert_uint8_array_to_ascii_string(arr_out, NB_BYTES_128_BITS, str_out);
-	if(strcmp(str_out, "ABCDEFGHIJKLMNOP") != 0) {
-		return CODE_FAIL;
-	}
-
-	return CODE_SUCCESS;
+int test_len0x16_get0() {
+	printf("######## Test: length 0*16 bytes, get block 0 ########\n");
+	return test_generic("", 0, "");
 }
 
-int test3() {
-	printf("######## Test 3 get block 0, size 2 blocks ########\n");
-	char str[] = "ABCDEFGHIJKLMNOPQRSTUVWXZ1234567";
-	size_t size = strlen(str);
-	printf("size %zu\n", size);
-	uint8_t arr_in[size];
-	convert_ascii_string_to_uint8_array(str, arr_in, size);
-	uint8_t arr_out[NB_BYTES_128_BITS];
-
-	get_block_exp(arr_in, arr_out, size, 0);
-
-	printf("Expected output: ABCDEFGHIJKLMNOP\n");
-	printf("Actual output:   "); print_uint8_array_as_ascii(arr_out, NB_BYTES_128_BITS, false);
-	
-	char str_out[NB_BYTES_128_BITS];
-	convert_uint8_array_to_ascii_string(arr_out, NB_BYTES_128_BITS, str_out);
-	if(strcmp(str_out, "ABCDEFGHIJKLMNOP") != 0) {
-		return CODE_FAIL;
-	}
-
-	return CODE_SUCCESS;
+int test_len0x16p9_get0() {
+	printf("######## Test: length 0*16+9 bytes, get block 0 ########\n");
+	return test_generic("abcdefghi", 0, "abcdefghi");
 }
 
-int test4() {
-	printf("######## Test 4 get block 1, size 2 blocks ########\n");
-	char str[] = "ABCDEFGHIJKLMNOPQRSTUVWXZ1234567";
-	size_t size = strlen(str);
-	printf("size %zu\n", size);
-	uint8_t arr_in[size];
-	convert_ascii_string_to_uint8_array(str, arr_in, size);
-	uint8_t arr_out[NB_BYTES_128_BITS];
-
-	get_block_exp(arr_in, arr_out, size, 1);
-
-	printf("Expected output: QRSTUVWXZ1234567\n");
-	printf("Actual output:   "); print_uint8_array_as_ascii(arr_out, NB_BYTES_128_BITS, false);
-	
-	char str_out[NB_BYTES_128_BITS];
-	convert_uint8_array_to_ascii_string(arr_out, NB_BYTES_128_BITS, str_out);
-	if(strcmp(str_out, "QRSTUVWXZ1234567") != 0) {
-		return CODE_FAIL;
-	}
-
-	return CODE_SUCCESS;
+int test_len1x16_get0() {
+	printf("######## Test: length 1*16 bytes, get block 0 ########\n");
+	return test_generic("abcdefghijklmnop", 0, "abcdefghijklmnop");
 }
 
-int test5() {
-	printf("######## Test 5 get block 0, size between 1 and 2 blocks ########\n");
-	char str[] = "ABCDEFGHIJKLMNOPQRSTUVWXZ12";
-	size_t size = strlen(str);
-	printf("size %zu\n", size);
-	uint8_t arr_in[size];
-	convert_ascii_string_to_uint8_array(str, arr_in, size);
-	uint8_t arr_out[NB_BYTES_128_BITS];
-
-	get_block_exp(arr_in, arr_out, size, 0);
-
-	printf("Expected output: ABCDEFGHIJKLMNOP\n");
-	printf("Actual output:   "); print_uint8_array_as_ascii(arr_out, NB_BYTES_128_BITS, false);
-	
-	char str_out[NB_BYTES_128_BITS];
-	convert_uint8_array_to_ascii_string(arr_out, NB_BYTES_128_BITS, str_out);
-	if(strcmp(str_out, "ABCDEFGHIJKLMNOP") != 0) {
-		return CODE_FAIL;
-	}
-
-	return CODE_SUCCESS;
+int test_len1x16p4_get0() {
+	printf("######## Test: length 1*16+4 bytes, get block 0 ########\n");
+	return test_generic("abcdefghijklmnopqrst", 0, "abcdefghijklmnop");
+}
+int test_len1x16p4_get1() {
+	printf("######## Test: length 1*16+4 bytes, get block 1 ########\n");
+	return test_generic("abcdefghijklmnopqrst", 1, "qrst");
 }
 
-int test6() {
-	printf("######## Test 6 get block 1, size between 1 and 2 blocks ########\n");
-	char str[] = "ABCDEFGHIJKLMNOPQRSTUVWXZ12";
-	size_t size = strlen(str);
-	printf("size %zu\n", size);
-	uint8_t arr_in[size];
-	convert_ascii_string_to_uint8_array(str, arr_in, size);
-	char expected_str[] = "QRSTUVWXZ12";
-	size_t len_expected = strlen(expected_str);
-	uint8_t arr_out[len_expected];
 
-	get_block_exp(arr_in, arr_out, size, 1);
+int test_len3x16_get0() {
+	printf("######## Test: length 3*16 bytes, get block 0 ########\n");
+	return test_generic("abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKL", 0, "abcdefghijklmnop");
+}
+int test_len3x16_get1() {
+	printf("######## Test: length 3*16 bytes, get block 1 ########\n");
+	return test_generic("abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKL", 1, "qrstuvwxyz123456");
+}
+int test_len3x16_get2() {
+	printf("######## Test: length 3*16 bytes, get block 2 ########\n");
+	return test_generic("abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKL", 2, "7890ABCDEFGHIJKL");
+}
+int test_len3x16_get3() {
+	printf("######## Test: length 3*16 bytes, get block 3 ########\n");
+	return test_generic("abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKL", 3, "");
+}
 
-	printf("Expected output: QRSTUVWXZ12\n");
-	printf("Actual output:   "); print_uint8_array_as_ascii(arr_out, len_expected, false);
-	
-	char str_out[len_expected];
-	convert_uint8_array_to_ascii_string(arr_out, len_expected, str_out);
-	if(strcmp(str_out, expected_str) != 0) {
-		return CODE_FAIL;
-	}
+int test_len3x16p7_get0() {
+	printf("######## Test: length 3*16+7 bytes, get block 0 ########\n");
+	return test_generic("abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRS", 0, "abcdefghijklmnop");
+}
+int test_len3x16p7_get1() {
+	printf("######## Test: length 3*16+7 bytes, get block 1 ########\n");
+	return test_generic("abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRS", 1, "qrstuvwxyz123456");
+}
+int test_len3x16p7_get2() {
+	printf("######## Test: length 3*16+7 bytes, get block 2 ########\n");
+	return test_generic("abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRS", 2, "7890ABCDEFGHIJKL");
+}
+int test_len3x16p7_get3() {
+	printf("######## Test: length 3*16+7 bytes, get block 3 ########\n");
+	return test_generic("abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRS", 3, "MNOPQRS");
+}
+int test_len3x16p7_get4() {
+	printf("######## Test: length 3*16+7 bytes, get block 4 ########\n");
+	return test_generic("abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRS", 4, "");
+}
 
-	return CODE_SUCCESS;
+int test_len6x16_get4() {
+	printf("######## Test: length 6*16 bytes, get block 4 ########\n");
+	return test_generic("abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz12345678", 4, "cdefghijklmnopqr");
+}
+int test_len6x16p2_get5() {
+	printf("######## Test: length 6*16 bytes, get block 5 ########\n");
+	return test_generic("abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890", 5, "stuvwxyz12345678");
+}
+int test_len6x16p2_get6() {
+	printf("######## Test: length 6*16 bytes, get block 6 ########\n");
+	return test_generic("abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890", 6, "90");
 }
 
 int main()
 {
-	print_test_return_status(test1());
-	print_test_return_status(test2());
-	print_test_return_status(test3());
-	print_test_return_status(test4());
-	print_test_return_status(test5());
-	print_test_return_status(test6());
+	print_test_return_status(test_len0x16_get0());
+	print_test_return_status(test_len0x16p9_get0());
+
+	print_test_return_status(test_len1x16_get0());
+	print_test_return_status(test_len1x16p4_get0());
+	print_test_return_status(test_len1x16p4_get1());
+
+	print_test_return_status(test_len3x16_get0());
+	print_test_return_status(test_len3x16_get1());
+	print_test_return_status(test_len3x16_get2());
+	print_test_return_status(test_len3x16_get3());
+	print_test_return_status(test_len3x16p7_get0());
+	print_test_return_status(test_len3x16p7_get1());
+	print_test_return_status(test_len3x16p7_get2());
+	print_test_return_status(test_len3x16p7_get3());
+	print_test_return_status(test_len3x16p7_get4());
+
+	print_test_return_status(test_len6x16_get4());
+	print_test_return_status(test_len6x16p2_get5());
+	print_test_return_status(test_len6x16p2_get6());
 
 	return 0;
 }
